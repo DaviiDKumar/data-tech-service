5905bf0d5f5da036b5452a1249840f6d8aee561e
DaviDChoudharY <122169398+DaviDChoudharY@users.noreply.github.com>
2026-05-15 17:58:40 +0530

    update

--- store/useAdminStore.js
+++ store/useAdminStore.js
@@ -1,12 +1,9 @@
 // store/useAdminStore.js
-import { create } from 'zustand'; // ← Add this line!
-
 export const useAdminStore = create((set) => ({
-  masterPool: [],
+  masterPool: [], // Saare 'Resume' Master entries
   uploadProgress: 0,
-  adminStats: null,
-
+  
   setMasterPool: (data) => set({ masterPool: data }),
   updateUploadProgress: (val) => set({ uploadProgress: val }),
-  setAdminStats: (data) => set({ adminStats: data }),
 }));
+
